8a779cf9f997c12e820f043fb1e5f3c3a69964a7
Bruno Enrique' work <bruno@vixta.ai>
2022-04-26 22:56:59 -0300

    Add new methods

--- script.js
+++ script.js
@@ -1,49 +1,86 @@
-// Js code to make color box enable or disable
-let colorIcons = document.querySelector(".color-icon"),
-icons = document.querySelector(".color-icon .icons");
+const addValuesToTable = (object) => {
+  const initialTable = document.querySelector(".initial-table");
+  object.forEach((i) => {
+    let tr = document.createElement("tr");
 
-icons.addEventListener("click" , ()=>{
-  colorIcons.classList.toggle("open");
-})
+    const cell1 = tr.insertCell(0);
+    const cell2 = tr.insertCell(1);
+    const cell3 = tr.insertCell(2);
+    const cell4 = tr.insertCell(3);
 
-// getting all .btn elements
-let buttons = document.querySelectorAll(".btn");
+    cell1.innerHTML = i.taste;
+    cell2.innerHTML = `R$${i.sixParts}`;
+    cell3.innerHTML = `R$${i.eightParts}`;
+    cell4.innerHTML = `${i.validDays}`;
 
-for (var button of buttons) {
-  button.addEventListener("click", (e)=>{ //adding click event to each button
-    let target = e.target;
-
-    let open = document.querySelector(".open");
-    if(open) open.classList.remove("open");
-
-    document.querySelector(".active").classList.remove("active");
-    target.classList.add("active");
-
-    // js code to switch colors (also day night mode)
-    let root = document.querySelector(":root");
-    let dataColor = target.getAttribute("data-color"); //getting data-color values of clicked button
-    let color = dataColor.split(" "); //splitting each color from space and make them array
-
-    //passing particular value to a particular root variable
-    root.style.setProperty("--white", color[0]);
-    root.style.setProperty("--black", color[1]);
-    root.style.setProperty("--nav-main", color[2]);
-    root.style.setProperty("--switchers-main", color[3]);
-    root.style.setProperty("--light-bg", color[4]);
-
-    let iconName = target.className.split(" ")[2]; //getting the class name of icon
-
-    let coloText = document.querySelector(".home-content span");
-
-    if(target.classList.contains("fa-moon")){ //if icon name is moon
-      target.classList.replace(iconName, "fa-sun") //replace it with the sun
-      colorIcons.style.display = "none";
-      coloText.classList.add("darkMode");
-    }else if (target.classList.contains("fa-sun")) { //if icon name is sun
-      target.classList.replace("fa-sun", "fa-moon"); //replace it with the sun
-      colorIcons.style.display = "block";
-      coloText.classList.remove("darkMode");
-      document.querySelector(".btn.blue").click();
-    }
+    initialTable.appendChild(tr);
   });
-}
+};
+
+let initialValues = [
+  {
+    taste: "Mussarela",
+    sixParts: "29,99",
+    eightParts: "38,99",
+    validDays: "Seg a Dom",
+  },
+  {
+    taste: "Calabresa",
+    sixParts: "29,99",
+    eightParts: "38,99",
+    validDays: "Ter a Dom",
+  },
+  {
+    taste: "Portuguesa",
+    sixParts: "29,99",
+    eightParts: "38,99",
+    validDays: "Ter a Dom",
+  },
+  {
+    taste: "Marguerita",
+    sixParts: "31,99",
+    eightParts: "39,99",
+    validDays: "Seg a Dom",
+  },
+];
+
+(function () {
+  const initialTable = document.querySelector(".initial-table");
+  let tr = document.createElement("tr");
+
+  const cell1 = tr.insertCell(0);
+  const cell2 = tr.insertCell(1);
+  const cell3 = tr.insertCell(2);
+  const cell4 = tr.insertCell(3);
+
+  cell1.innerHTML = "Sabor";
+  cell2.innerHTML = "6 pedaços";
+  cell3.innerHTML = "8 pedaços";
+  cell4.innerHTML = "Dias válidos";
+
+  initialTable.appendChild(tr);
+
+  addValuesToTable(initialValues);
+})();
+
+(function () {
+  const submitBtn = document.querySelector("#submit");
+  submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const taste = document.querySelector("#taste").value;
+    const sixParts = document.querySelector("#six-parts").value;
+    const eightParts = document.querySelector("#eight-parts").value;
+    const validDays = document.querySelector("#valid-days").value;
+
+    addValuesToTable([{ taste, sixParts, eightParts, validDays }]);
+  });
+})();
+
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 3) {
+    counter = 1;
+  }
+}, 5000);
