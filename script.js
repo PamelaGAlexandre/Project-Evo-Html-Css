d36310419950a8bc3410aff1608b9b3dcf7e8af3
PamelaGAlexandre <89876084+PamelaGAlexandre@users.noreply.github.com>
2022-05-03 18:51:49 -0300

    add bootstrap

--- script.js
+++ script.js
@@ -1,72 +1,80 @@
-const addValuesToTable = (object) => { //adiciona o objeto na tabela
-  const initialTable = document.querySelector(".initial-table");
-  object.forEach((i) => {
-    let tr = document.createElement("tr"); //crio o elemento TR
+let cont = 1;
 
-    const cell1 = tr.insertCell(0); //adiciono 4 células dentro do elemento tr
+const addValuesToTable = (object) => { //funcao que adiciona valores na tabela
+  const initialTable = document.querySelector(".table-values"); //pega o tbody do html 
+  object.forEach((i) => { 
+    let tr = document.createElement("tr"); //cria um elemento do tipo html
+
+    const cell1 = tr.insertCell(0); //adiciona 4 células (colunas)
     const cell2 = tr.insertCell(1);
     const cell3 = tr.insertCell(2);
     const cell4 = tr.insertCell(3);
 
-    cell1.innerHTML = i.taste; // adiciono nas células os valores de dentro do objeto
-    cell2.innerHTML = `R$${i.sixParts}`;
-    cell3.innerHTML = `R$${i.eightParts}`;
-    cell4.innerHTML = `${i.validDays}`;
+    cell1.innerHTML = cont; //adiciona na primeira célula o id
+    cell2.innerHTML = `${i.taste}`; //na segunda célula o sabor
+    cell3.innerHTML = `R$${Number(i.value).toFixed(2)}`; // na terceira célula o preço
+    cell4.innerHTML = `${i.ingredients}`; //na quarta célula os ingredientes
 
-    initialTable.appendChild(tr);
+    initialTable.appendChild(tr); 
+    cont++;
   });
 };
 
 (function () {
-  const fixedValues = [
-    {
-    taste: "Sabor",
-    sixParts: "6 pedaços",
-    eightParts: "8 pedaços",
-    validDays: "Dias válidos",
-    }
-  ];
-
-  let initialValues = [
+  let fidexValues = [ //valores fixos
     {
       taste: "Mussarela",
-      sixParts: "29,99",
-      eightParts: "38,99",
-      validDays: "Seg a Dom",
+      value: 29.99,
+      ingredients: "queijo, azeitona, oregano, molho de tomate",
     },
     {
       taste: "Calabresa",
-      sixParts: "29,99",
-      eightParts: "38,99",
-      validDays: "Ter a Dom",
+      value: 29.99,
+      ingredients: "calabresa, azeitona, oregano, cebola, molho de tomate",
     },
     {
-      taste: "Portuguesa",
-      sixParts: "29,99",
-      eightParts: "38,99",
-      validDays: "Ter a Dom",
-    },
-    {
-      taste: "Marguerita",
-      sixParts: "31,99",
-      eightParts: "39,99",
-      validDays: "Seg a Dom",
+      taste: "Bauru",
+      value: 29.99,
+      ingredients: "mussarela, azeitona, oregano, presunto, tomate, molho tomate",
     },
   ];
 
-  addValuesToTable(fixedValues);
-  addValuesToTable(initialValues);
+  addValuesToTable(fidexValues); //adiciono os valores fixos
 })();
 
-(function () {
-  const submitBtn = document.querySelector("#submit");
-  submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    const taste = document.querySelector("#taste").value;
-    const sixParts = document.querySelector("#six-parts").value;
-    const eightParts = document.querySelector("#eight-parts").value;
-    const validDays = document.querySelector("#valid-days").value;
+(function () { //funcao que adiciona valores dinamicos na tabela
+  const submitBtn = document.querySelector(".btn.btn-primary"); //pego o botão  de submit
+  submitBtn.addEventListener("click", (e) => { // crio um event listener pro botão de tipo click
+    e.preventDefault(); //impede que ao clicar no botão de submit, ele resete a página
+    const taste = document.querySelector(".taste-type").value; //pega os valores dos inputs
+    let value = document.querySelector(".value-type").value;
+    const ingredients = document.querySelector(".ingredients-type").value;
 
-    addValuesToTable([{ taste, sixParts, eightParts, validDays }]);
+    if(!taste || !value || !ingredients){ //verifica se faltou algum valor dos inputs
+      window.alert("Faltou algum campo!");
+      return;
+    }
+
+    if(isNaN(value)) { //verfica se o input de valor é do tipo número
+      window.alert("Digite um valor válido no campo 'valor'!");
+      document.querySelector(".value-type").value = '';
+      return;
+    }
+  
+   value = Number(value) //transformar o input de valor em numero
+   addValuesToTable([{ taste, value, ingredients }]); //chama a função de adicionar valores na tabela 
+
+   document.querySelector(".taste-type").value = ''; //esvazia os campos dos inputs após serem adicionados na tabela
+   document.querySelector(".value-type").value = '';
+   document.querySelector(".ingredients-type").value = '';
   });
 })();
+
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 3) {
+    counter = 1;
+  }
+}, 5000);
